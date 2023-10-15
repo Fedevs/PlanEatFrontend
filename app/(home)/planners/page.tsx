371392bd3e8f8dc6cd7ae6201e7f66ee19881a14
Federico <fededevsit@gmail.com>
2023-10-15 19:43:38 +0930

    changing position sticky to absoule and adjusting margins

--- app/(home)/planners/page.tsx
+++ app/(home)/planners/page.tsx
@@ -1,13 +1,15 @@
 import getPlanners from "@/app/hooks/getPlanners";
 import { PlannerInterface } from "@/types/types";
 import PlannerCard from "@/app/components/PlannerCard/PlannerCard";
+import "./planners.scss";
 
 export default async function Planners() {
   const planners = await getPlanners();
+  const test = [...planners, ...planners];
 
   return (
-    <section className='mt-3'>
-      {planners.map((planner: PlannerInterface) => (
+    <section className='planners my-3 h-100'>
+      {test.map((planner: PlannerInterface) => (
         <PlannerCard key={planner.id} {...planner} />
       ))}
     </section>
