4ff12478764193f4ca6d8b7038bd4c5cd5b4e77d
Federico <fededevsit@gmail.com>
2023-10-15 20:41:39 +0930

    fix scroll behaviour

--- app/(home)/planners/page.tsx
+++ app/(home)/planners/page.tsx
@@ -1,13 +1,12 @@
 import getPlanners from "@/app/hooks/getPlanners";
 import { PlannerInterface } from "@/types/types";
 import PlannerCard from "@/app/components/PlannerCard/PlannerCard";
-import "./planners.scss";
 
 export default async function Planners() {
   const planners = await getPlanners();
 
   return (
-    <section className='planners my-3 h-100'>
+    <section className='my-3 h-100'>
       {planners.map((planner: PlannerInterface) => (
         <PlannerCard key={planner.id} {...planner} />
       ))}
