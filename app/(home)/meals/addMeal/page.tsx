13792ef6f1b44cf8054be5aabf020e39e0fb8b47
Federico <fededevsit@gmail.com>
2023-10-19 14:49:46 +0930

    dropdown working

--- app/(home)/meals/addMeal/page.tsx
+++ app/(home)/meals/addMeal/page.tsx
@@ -1,53 +1,66 @@
 "use client";
 
-import { useState } from "react";
-import "./addMeal.scss";
+import React, { useEffect, useState } from "react";
+import { IngredientInterface } from "@/types/types";
 import Dropdown from "@/app/components/Dropdown/Dropdown";
-
-type Ingredient = string;
-
-const allIngredients: Ingredient[] = [
-  "Orange",
-  "Capsicum",
-  "Cucumber",
-  "Orange",
-  "Capsicum",
-  "Cucumber",
-];
+import "./addMeal.scss";
 
 export default function AddMeal() {
   const [name, setName] = useState<string>("");
-  const [selectedIngredients, setSelectedIngredients] = useState<Ingredient[]>(
+  const [allIngredients, setAllIngredients] = useState<IngredientInterface[]>(
     []
   );
+  const [filteredIngredients, setFilteredIngredients] = useState<
+    IngredientInterface[]
+  >([]);
+  const [selectedIngredients, setSelectedIngredients] = useState<
+    IngredientInterface[]
+  >([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
 
-  const handleIngredientChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const value: Ingredient = event.target.value;
-    if (selectedIngredients.includes(value)) {
-      setSelectedIngredients(
-        selectedIngredients.filter((item) => item !== value)
-      );
-    } else {
-      setSelectedIngredients([...selectedIngredients, value]);
-    }
-  };
+  useEffect(() => {
+    fetch("/api/ingredients")
+      .then((res) => res.json())
+      .then((res) => {
+        setAllIngredients(res);
+        setFilteredIngredients(res);
+      });
+  }, []);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+
+    const filteredIngredients = allIngredients.filter((ingredient) =>
+      ingredient.name.toLowerCase().includes(searchTerm)
+    );
+
+    setFilteredIngredients(filteredIngredients);
   };
 
-  const filteredIngredients: Ingredient[] = allIngredients.filter(
-    (ingredient) => ingredient.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filterIngredientsList = (ingredient: IngredientInterface) => {
+    const filterList = filteredIngredients.filter(
+      (item) => item !== ingredient
+    );
+    setFilteredIngredients(filterList);
+  };
+
+  const handleIngredientsChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    ingredient: IngredientInterface
+  ) => {
+    const updatedSelection = selectedIngredients.includes(ingredient)
+      ? selectedIngredients.filter((item) => item !== ingredient)
+      : [...selectedIngredients, ingredient];
+    filterIngredientsList(ingredient);
+    setSelectedIngredients(updatedSelection);
+  };
 
   return (
     <div className='add-meal'>
       <h1>Add Meal</h1>
       <form onSubmit={() => {}} className='align-self-start w-100'>
-        <div className='flex w-100 justify-start gap-2 mb-4'>
+        <div className='flex w-100 justify-start gap-2 mb-6'>
           <label htmlFor='name'>Name</label>
           <input
             className='input p-2 my-1'
@@ -59,23 +72,33 @@ export default function AddMeal() {
           />
         </div>
         <div className='flex-column align-start gap-2 w-100'>
-          <label htmlFor='ingredients'>Ingredient</label>
+          <label htmlFor='ingredients'>Ingredients</label>
           <Dropdown
+            debug={false}
             handleSearchChange={handleSearchChange}
+            id={"ingredients"}
             searchTerm={searchTerm}
             placeholder='Filter ingredients...'
           >
-            {filteredIngredients.map((ingredient, index) => (
-              <label key={index} className='dropdown-option'>
-                <input
-                  type='checkbox'
-                  value={ingredient}
-                  onChange={handleIngredientChange}
-                  checked={selectedIngredients.includes(ingredient)}
-                />
-                {ingredient}
-              </label>
-            ))}
+            {filteredIngredients.length ? (
+              filteredIngredients.map((ingredient) => (
+                <label key={ingredient.id} className='dropdown-option py-1'>
+                  <input
+                    type='checkbox'
+                    style={{ display: "none" }}
+                    value={ingredient.name}
+                    // checked={selectedIngredients.includes(ingredient)}
+                    // checked={alert(ingredient.name)}
+                    onChange={(e) => handleIngredientsChange(e, ingredient)}
+                  />
+                  {ingredient.name}
+                </label>
+              ))
+            ) : (
+              <p>
+                <b>No ingredients found</b>
+              </p>
+            )}
           </Dropdown>
         </div>
       </form>
--- app/components/Dropdown/Dropdown.tsx
+++ app/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Dropdown.scss";
 
 interface DropdownProps {
@@ -6,24 +6,48 @@ interface DropdownProps {
   handleSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder: string;
   children: React.ReactNode;
+  id: string;
 }
 
 export default function Dropdown(props: DropdownProps) {
   const [isFocused, setIsFocused] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleInputFocus = () => {
     setIsFocused(true);
   };
 
-  const handleInputBlur = () => {
+  const handleInputBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (
+      dropdownRef.current &&
+      dropdownRef.current.contains(event.target as Node)
+    ) {
+      return;
+    }
     setIsFocused(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsFocused(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className='dropdown flex w-100'>
+    <div className='dropdown flex w-100' ref={dropdownRef}>
       <input
         type='text'
-        className='search-input w-100 p-2'
+        id={props.id}
+        className={`search-input w-100 p-2 ${isFocused && "focus-input"}`}
         placeholder={props.placeholder}
         value={props.searchTerm}
         onChange={props.handleSearchChange}
